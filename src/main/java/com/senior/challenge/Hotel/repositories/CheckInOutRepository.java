09841fe0d00ce376dfd76b0a2cc647b4de25f743
BrunoHM <brunoh.murta@gmail.com>
2021-02-01 04:54:48 -0300

    Adicionado chamada e query para alterar/concluir checkInOut do hóspede

--- src/main/java/com/senior/challenge/Hotel/repositories/CheckInOutRepository.java
+++ src/main/java/com/senior/challenge/Hotel/repositories/CheckInOutRepository.java
@@ -1,5 +1,20 @@
 package com.senior.challenge.Hotel.repositories;
 
-public interface CheckInOutRepository {
+import java.time.OffsetDateTime;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.senior.challenge.Hotel.entities.CheckInOut;
+
+@Repository
+public interface CheckInOutRepository extends JpaRepository<CheckInOut, Long>{
+	
+	//Altera o checkInOut do hóspede
+	@Modifying
+	@Query(value = "UPDATE dtbhotel.checkinout SET \"departureDate\" = ?2, \"billingValue\" = ?3 WHERE \"id\" = ?1 ;", nativeQuery = true)
+	public int updateCheckInOut(Long idCheckInOut, OffsetDateTime departureDate, double finalBillingValue);
+	
 }
